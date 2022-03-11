0906efe9f5fe6ac81e4e41c7df69066b6edf0000
vichu555 <vichuvichithra87@gmail.com>
2022-03-11 15:50:44 +0530

    hoi

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/hostel',async(req,res)=>     //api creation
 {
     
-    console.log('Hello World!');
+    console.log('Hello balaji');
 
     //res.send("Balaji viyan");
     const hostelList = await hostel.find();
